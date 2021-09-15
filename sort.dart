112b11bc2efd2eff3377f8a5b8a85fc1a5fb09fc
Akshat pujara <72406086+ak22-pujara@users.noreply.github.com>
2021-09-15 11:28:23 +0530

    Add files via upload

---
+++ sort.dart
@@ -0,0 +1,99 @@
+void main(){
+  
+  List<String> arr= [
+'Kalpit Seksariya',                 
+'Gaurang Gajera',
+'Bollam saiprakash',
+'Dhruv Vaghela',
+'Jaydeep Dhamecha',
+'Jigar Pandya',
+'Riddhi Bhavsar',
+'Ankit Jadav',
+'Jayesh Gurudayalani',
+'Charmi Goswami',
+'Manish Mundra ',
+'Siddarth Koshiyar ',
+'Anand Sharma ',
+'Chirag Rami ',
+'Akash Thakkar',
+'Khushi Mundra',
+'Ami Thakkar',
+'Ashutosh Dave',
+'Dhruv Samani',
+'Sajid Shaikh ',
+'QURESHI MOHAMMADJALAL',
+'Devang Patel',
+'Pravin Fofariya ',
+'Raj Singhala',
+'Mahesh Kumar Dey',
+'Akshat Pujara',
+'Shivam Pathak',
+'Suhas Vaishnav',
+'Nirbhay Goswami',
+'Manjeet Kaur Bhasin',
+'Yash Parmar',
+'Rutvik Dhami',
+'Harsh Shah',
+'Farhan Shaikh ',
+'Priyank Jamvecha',
+'Rutvik Jamvecha',
+'Raj Singhala',
+'SRINIVAS KALYAN MORLA',
+'Madhupada Vinay',
+'Namrata Jenish kacha',
+'Nisarg Parikh',
+'Ruchi Kariya',
+'Jhanvi Vaishnav',
+'Dhruv Sanjaybhai Kikani ',
+'Kushal jain',
+'Renish Dudhat',
+'Dhruvil thakkar',
+'Snehal Prajapati',
+'Dahal sudip',
+'Yash vithani'   
+    
+];
+//print(arr);       
+  arr.shuffle();
+  var arr2 = arr.getRange(1,5);
+  arr.sort();
+  print(arr2);
+  
+  var arr3 = arr.getRange(6,10);
+  arr.sort();
+  print(arr3);
+  
+  var arr4 = arr.getRange(11,15);
+  arr.sort();
+  print(arr4);
+
+  var arr5 = arr.getRange(15,20);
+  arr.sort();
+  print(arr5);
+  
+  var arr6 = arr.getRange(20,25);
+  arr.sort();
+  print(arr6);
+  
+  var arr7 = arr.getRange(26,30);
+  arr.sort();
+  print(arr7);
+  
+  var arr8 = arr.getRange(31,35);
+  arr.sort();
+  print(arr8);
+  
+  var arr9 = arr.getRange(36,40);
+  arr.sort();
+  print(arr9);
+  
+  var arr10 = arr.getRange(41,45);
+  arr.sort();
+  print(arr10);
+  
+  var arr11 = arr.getRange(46,50);
+  arr.sort();
+  print(arr11);
+  
+  
+}
